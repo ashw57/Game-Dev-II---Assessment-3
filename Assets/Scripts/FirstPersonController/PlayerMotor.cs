0e99b9df809187b8f53523e82de19f327dcfc3d3
ashw57 <nugoot57@gmail.com>
2025-04-29 17:27:25 +1000

    Finished Input Code, Scrapped Freddy

--- Assets/Scripts/FirstPersonController/PlayerMotor.cs
+++ Assets/Scripts/FirstPersonController/PlayerMotor.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
-using UnityEngine.UIElements;
-using UnityEngine.Windows;
+using UnityEngine.InputSystem;
 
 public class PlayerMotor : MonoBehaviour
 {
@@ -11,6 +10,8 @@ public class PlayerMotor : MonoBehaviour
     private Animator animator;
     private AudioSource audioSource;
     private Camera cam;
+    private PlayerInput playerInput;
+    private PlayerInput.OnFootActions input;
     private Vector3 playerVelocity;
 
     // ------------------- //
@@ -42,18 +43,36 @@ public class PlayerMotor : MonoBehaviour
     private bool readyToAttack = true;
     int attackCount;
 
+    // ------------------ //
+    //     ANIMATIONS     //
+    // ------------------ //
+
+    public const string IDLE = "DS_onehand_idle_A";
+    public const string WALK = "DS_onehand_walk";
+    public const string ATTACK1 = "DS_onehand_attack_A";
+    public const string ATTACK2 = "DS_onehand_attack_B";
+
+    string currentAnimationState;
+
     void Start()
     {
         controller = GetComponent<CharacterController>();
         animator = GetComponentInChildren<Animator>();
         cam = GetComponentInChildren<Camera>();
+        audioSource = GetComponent<AudioSource>();
+
+        playerInput = new PlayerInput();
+        input = playerInput.OnFoot;
     }
 
     private void Update()
     {
         isGrounded = controller.isGrounded;
 
-        //SetAnimations();
+        // Continious Input
+        if (input.Attack.IsPressed()) Attack();
+
+        SetAnimations();
     }
 
     //This will receive the inputs for our Input Manager and apply them to our character controller.
@@ -86,8 +105,23 @@ public class PlayerMotor : MonoBehaviour
         Invoke(nameof(ResetAttack), attackSpeed);
         Invoke(nameof(AttackRaycast), attackDelay);
 
-        audioSource.pitch = Random.Range(0.9f, 1.1f);
-        audioSource.PlayOneShot(swordSwing);
+        if (audioSource != null)
+        {
+            audioSource.pitch = Random.Range(0.9f, 1.1f);
+            audioSource.PlayOneShot(swordSwing);
+        }
+        else Debug.LogError("AudioSourceNotFound");
+
+        if(attackCount == 0)
+        {
+            ChangeAnimationState(ATTACK1);
+            attackCount++;
+        }
+        else
+        {
+            ChangeAnimationState(ATTACK2);
+            attackCount = 0;
+        }
     }
 
     private void ResetAttack()
@@ -106,10 +140,35 @@ public class PlayerMotor : MonoBehaviour
 
     private void HitTarget(Vector3 pos)
     {
-        audioSource.pitch = 1;
-        audioSource.PlayOneShot(hitSound);
-
+        if (audioSource != null)
+        {
+            audioSource.pitch = 1;
+            audioSource.PlayOneShot(hitSound);
+        }
+        else Debug.LogError("AudioSourceNotFound");
         GameObject GO = Instantiate(hitEffect, pos, Quaternion.identity);
         Destroy(GO, 20);
+    }
+
+    public void ChangeAnimationState(string newState)
+    {
+        // Stop the same animation from interrupting with itself
+        if (currentAnimationState == newState) return;
+
+        // Play the animation
+        currentAnimationState = newState;
+        animator.CrossFadeInFixedTime(currentAnimationState, 0.2f);
+    }
+
+    private void SetAnimations()
+    {
+        // If player is not attacking
+
+        if (!attacking)
+        {
+            if (playerVelocity.x == 0 && playerVelocity.z == 0)
+            { ChangeAnimationState(IDLE); }
+            else { ChangeAnimationState(WALK); }
+        }
     }
 }
